78d0c3c634da6896bf69f9bcae2bfc696f926ffa
Build <support@mindpowered.dev>
2020-11-29 05:58:39 +0000

    Version 0.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='mindpowered-sqlalchemy-persistence',
+    version='0.0.1',
+    author="Mind Powered Corporation",
+    author_email="support@mindpowered.dev",
+    license="CPAL-1.0",
+    url="https://mindpowered.dev/",
+    description="SQLAlchemy Persistence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['persistence'],
+    packages=['mindpowered_sqlalchemy_persistence'],
+    package_dir={'mindpowered_sqlalchemy_persistence': 'wrappers'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'mindpowered-maglev',
+        'SQLAlchemy'
+    ],
+)
