e8406fd9961c94c525d60d31342ca7fb11731810
Build <support@mindpowered.dev>
2020-11-29 06:24:27 +0000

    Version 0.0.2

--- persistence.py
+++ persistence.py
@@ -1,6 +1,6 @@
 import maglev
 
-class Persistence:
+class persistence_Persistence:
 	def __init__(self, bus):
 		self.pybus = maglev.maglev_MagLevPy(bus)
 		self.pybus.register('Persistence.EnglishAuction.Auction.CreateNew', self.Persistence_EnglishAuction_Auction_CreateNew)
@@ -11,19 +11,19 @@ class Persistence:
 		self.pybus.register('Persistence.EnglishAuction.Bid.CountForAuction', self.Persistence_EnglishAuction_Bid_CountForAuction)
 		self.pybus.register('Persistence.EnglishAuction.Bid.FindByHighestPriceForAuction', self.Persistence_EnglishAuction_Bid_FindByHighestPriceForAuction)
 		self.pybus.register('Persistence.EnglishAuction.Bid.New', self.Persistence_EnglishAuction_Bid_New)
-	def Persistence_EnglishAuction_Auction_CreateNew(args):
+	def Persistence_EnglishAuction_Auction_CreateNew(self, args):
 		return "123"
-	def Persistence_EnglishAuction_Auction_FindById(args):
+	def Persistence_EnglishAuction_Auction_FindById(self, args):
 		return null
-	def Persistence_EnglishAuction_Auction_FindStarting(args):
+	def Persistence_EnglishAuction_Auction_FindStarting(self, args):
 		return null;
-	def Persistence_EnglishAuction_Auction_FindEnding(args):
+	def Persistence_EnglishAuction_Auction_FindEnding(self, args):
 		return null
-	def Persistence_EnglishAuction_Auction_FindOpen(args):
+	def Persistence_EnglishAuction_Auction_FindOpen(self, args):
 		return null
-	def Persistence_EnglishAuction_Bid_CountForAuction(args):
+	def Persistence_EnglishAuction_Bid_CountForAuction(self, args):
 		return null
-	def Persistence_EnglishAuction_Bid_FindByHighestPriceForAuction(args):
+	def Persistence_EnglishAuction_Bid_FindByHighestPriceForAuction(self, args):
 		return null
-	def Persistence_EnglishAuction_Bid_New(args):
+	def Persistence_EnglishAuction_Bid_New(self, args):
 		return null
